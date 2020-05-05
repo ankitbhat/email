141bea8e57794e26c991ead060bb9a25e1f0353a
Ankit Bhat <ankitbhatpro@gmail.com>
2020-05-06 04:44:10 +0530

    Added password with atleast one numeric value

--- emailid.sh
+++ emailid.sh
@@ -34,9 +34,9 @@ echo invalid phone number
 fi
 echo Enter password
 read password
-if [[ $password =~ ^([a-z0-9!@#%^&*]*)([A-Z]+)([a-z0-9!@#%^&*]*){8,}$ ]]
+if [[ $password =~ ^([a-zA-Z0-9!@#%^&*]*)([A-Z]+)([a-zA-Z0-9!@#%^&*]*){8,}$ ]]
 then
-	if [[ $password =~ ^([a-zA-Z!@#%^&*]*)([0-9]+)([a-zA-Z!@#%^&*]*){8,}$ ]]
+	if [[ $password =~ ^([a-zA-Z0-9!@#%^&*]*)([0-9]+)([a-zA-Z0-9!@#%^&*]*){8,}$ ]]
 	then
 	echo valid password
 	else
