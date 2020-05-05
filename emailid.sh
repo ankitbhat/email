3bd54a6a73f2a7cacc32df0d7c58b669480dca5a
Ankit Bhat <ankitbhatpro@gmail.com>
2020-05-06 01:12:37 +0530

    Added email id

--- emailid.sh
+++ emailid.sh
@@ -1,21 +1,21 @@
 #!/bin/bash -x
 shopt -s extglob
-#echo Enter First Name
-#read fname
-#if [[ $fname =~ (^[?A-Z][a-z]{2,}) ]]
-#then
-#echo valid name
-#else
-#echo invalid name
-#fi
-#echo Enter Last Name
-#read lname
-#if [[ $lname =~ (^[?A-Z][a-z]{2,}) ]]
-#then
-#echo valid name
-#else
-#echo invalid name
-#fi
+echo Enter First Name
+read fname
+if [[ $fname =~ (^[?A-Z][a-z]{2,}) ]]
+then
+echo valid name
+else
+echo invalid name
+fi
+echo Enter Last Name
+read lname
+if [[ $lname =~ (^[?A-Z][a-z]{2,}) ]]
+then
+echo valid name
+else
+echo invalid name
+fi
 echo Enter email id
 read email
 if [[ $email =~ ^[a-zA-Z0-9]+([._+-][a-zA-Z0-9]+)*@[a-zA-Z0-9]+.[a-zA-Z]{2,4}([.][a-zA-Z]{2})*$ ]] 
